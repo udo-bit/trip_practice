03c06798164f93998ed11b3f8ca585c0feda9e8e
udo-bit <enote_gxl@163.com>
2024-12-17 22:55:39 +0800

    banner

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trip_practice/dao/login_dao.dart';
+import 'package:trip_practice/widget/banner_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +10,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> lists = [
+    "https://img1.baidu.com/it/u=2311996924,724373457&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313",
+    "https://img0.baidu.com/it/u=778214893,3396635396&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=338",
+    "https://img2.baidu.com/it/u=152996483,2513007285&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500",
+    "https://img0.baidu.com/it/u=2942798871,4064395557&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500",
+  ];
+
   get _logout {
     return ElevatedButton(
         onPressed: () {
@@ -20,11 +28,12 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("首页"),
-        actions: [_logout],
-      ),
-      body: const Text('首页'),
-    );
+        appBar: AppBar(
+          title: const Text("首页"),
+          actions: [_logout],
+        ),
+        body: BannerWidget(
+          lists: lists,
+        ));
   }
 }
---
+++ lib/widget/banner_widget.dart
@@ -0,0 +1,73 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class BannerWidget extends StatefulWidget {
+  const BannerWidget({super.key, required this.lists});
+  final List<String> lists;
+
+  @override
+  State<BannerWidget> createState() => _BannerWidgetState();
+}
+
+class _BannerWidgetState extends State<BannerWidget> {
+  final CarouselSliderController _controller = CarouselSliderController();
+  int _current = 0;
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    return Stack(children: [
+      CarouselSlider(
+        carouselController: _controller,
+        items: widget.lists.map((item) => _tableItems(item, width)).toList(),
+        options: CarouselOptions(
+          height: 200,
+          viewportFraction: 1.0,
+          autoPlay: true,
+          onPageChanged: (int index, CarouselPageChangedReason reason) {
+            setState(() {
+              _current = index;
+            });
+          },
+        ),
+      ),
+      Positioned(
+        left: 0,
+        right: 0,
+        bottom: 5,
+        child: _indicator(),
+      )
+    ]);
+  }
+
+  Widget _tableItems(String item, double width) {
+    return GestureDetector(
+      onTap: () {},
+      child: Image.network(
+        item,
+        fit: BoxFit.cover,
+        width: width,
+      ),
+    );
+  }
+
+  _indicator() {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: widget.lists.asMap().entries.map((entry) {
+          return GestureDetector(
+            onTap: () {
+              _controller.jumpToPage(entry.key);
+            },
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 30),
+              height: 10,
+              width: 10,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: (Colors.white)
+                      .withOpacity(entry.key == _current ? 0.9 : 0.4)),
+            ),
+          );
+        }).toList());
+  }
+}
